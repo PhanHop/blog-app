5ddca3ff74bea370f182877d6a882da49c439728
PHANHOP-PC\admin <phanhop24022001@gmail.com>
2023-07-25 14:28:05 +0700

    Update header

--- src/components/common/Header/Header.tsx
+++ src/components/common/Header/Header.tsx
@@ -5,10 +5,10 @@ import Container from "../../Container";
 export default function Header() {
   const { data } = useGetListNews();
   return (
-    <Container className="flex items-center gap-x-5">
+    <Container className="flex items-center gap-x-5 sticky top-0 z-10 bg-white border-b-[1px] border-stone-200">
       {data?.map((item) => {
         return (
-          <div key={item?.id} className="py-3 font-medium">
+          <div key={item?.id} className="py-3 font-medium hover:cursor-pointer hover:text-blue-600">
             {item?.tenDanhMuc}
           </div>
         );
